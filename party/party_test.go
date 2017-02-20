1d98581b49bba901636d9d2f438abab19a91b05b
ConnorFoody <connorfoody@gmail.com>
2017-02-20 12:33:01 -0500

    created initial party

---
+++ party/party_test.go
@@ -0,0 +1,36 @@
+package party_test
+
+import (
+	"github.com/me-next/menext-backend/party"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestPartyUserAdd(t *testing.T) {
+	ownerUUID := party.UserUUID("1")
+	p := party.New(ownerUUID, "fred")
+
+	user := party.UserUUID("2")
+	assert.Nil(t, p.AddUser(user, "bob"))
+
+	// check that we can't remove someone who isn't there
+	assert.NotNil(t, p.RemoveUser("4"))
+
+	// can't double add users
+	assert.NotNil(t, p.AddUser(user, "bob"))
+
+	// remove users should be ok
+	assert.Nil(t, p.RemoveUser(user))
+
+	// can't double-remove
+	assert.NotNil(t, p.RemoveUser(user))
+
+	// can remove then add back in
+	assert.Nil(t, p.AddUser(user, "bob"))
+
+	// check that owner was inserted
+	assert.NotNil(t, p.AddUser(ownerUUID, "fred"))
+
+	// check that we can't remove owners
+	assert.NotNil(t, p.RemoveUser(ownerUUID))
+}
---
+++ party/user.go
@@ -0,0 +1,29 @@
+package party
+
+// User at a party
+type User struct {
+	name        string
+	permissions map[string]bool
+}
+
+// UserUUID is the uuid for a user
+type UserUUID string
+
+// NewUser with name
+func NewUser(name string) *User {
+	return &User{
+		name:        name,
+		permissions: make(map[string]bool),
+	}
+}
+
+// CanPerform checks if a user can perform an action
+func (u User) CanPerform(action string) bool {
+	canPerform, has := u.permissions[action]
+	return canPerform && has
+}
+
+// SetPermission sets a permission for an action
+func (u *User) SetPermission(action string, canPerform bool) {
+	u.permissions[action] = canPerform
+}

271ace18716360f5243fb6fbdea7df06b880caea
ConnorFoody <connorfoody@gmail.com>
2017-02-27 12:23:53 -0500

    implemented basic queue

--- queue/queue.go
+++ queue/queue.go
@@ -1,77 +1,18 @@
 package queue
 
-import (
-	"fmt"
-)
-
+// SongUUID is a universally unique identifier for a song
 type SongUUID string
 
+// Song data includes priority
 type Song struct {
 	Priority int
+	UUID     SongUUID
 }
 
-func NewSong(p int) Song{
-	return Song{Priority : p}
-}
-
-type Queue struct {
-	songList map[SongUUID]Song
-}
-
-func NewQueue() *Queue {
-	q := Queue{
-		songList: make(map[SongUUID]Song),
-	}
-	return &q
-}
-
-func (q *Queue) AddSong(songID SongUUID) error {
-	_, found := q.songList[songID]
-	if !found {
-		for _, s := range q.songList{
-			s.Priority ++
-		}
-		q.songList[songID] = NewSong(1)
-		return nil
-	}
-	return fmt.Errorf("song %s already in queue", songID)
-}
-
-func (q *Queue) RemoveSong(songID SongUUID) error {
-	_, found := q.songList[songID]
-	if !found {
-		return fmt.Errorf("song %s not in queue", songID)
-	}
-	q.decreaseAllAbove(songID)
-	delete(q.songList, songID)
-	return nil
-	
-}
-
-func(s Song) increasePriority(){
-	s.Priority++ 
-}
-
-func(s Song) decreasePriority(){
-	s.Priority--
-}
-
-func (q *Queue) increaseAllAbove(s SongUUID){
-	song := q.songList[s]
-	p := song.Priority
-	for _,i := range q.songList{
-		if p < i.Priority {
-			i.increasePriority()
-		}
-	}
-}
-
-func (q *Queue) decreaseAllAbove(s SongUUID){
-	song := q.songList[s]
-	p := song.Priority
-	for _,i := range q.songList{
-		if p < i.Priority {
-			i.decreasePriority()
-		}
-	}
+// Queue is the basic interface for a queue.
+// NOTE: the queue its self provides no threadsafety
+type Queue interface {
+	Add(Song) error
+	Remove(SongUUID) error
+	Pop() (Song, error)
 }
